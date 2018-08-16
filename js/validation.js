1c2635f315f684a789620aa122ff052dddc4776d
Ryan Rackemann <ryanrackemann@gmail.com>
2018-08-16 10:51:53 -0700

    Added validation.js
    initial publish

---
+++ js/validation.js
@@ -0,0 +1,5 @@
+function validate(){
+	document.getElementById("response").innerHTML = "<div class=\"w3-panel w3-leftbar w3-border-red w3-pale-red\">" +
+		"<p>I have a thick red top and bottom border and a pale-red background color.</p>" +
+	"</div>";
+}
