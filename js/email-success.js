c57c7efe1d49966c17d30b5411d3ce9544aaad27
Ryan Rackemann <ryanrackemann@gmail.com>
2018-08-29 23:25:12 -0700

    Display changer
    added js to change display value

--- js/email-success.js
+++ js/email-success.js
@@ -1,6 +1,7 @@
 function checkUrl(){
 	var url = window.location.href;
 	if(window.location.href.includes("success")){
+		document.getElementById("emailSuccess").style.display = "block";
 		document.getElementById("emailSuccess").innerHTML = "Thank you for your email. I will respond to you as soon as I can.";
 	}
 }
