02315441e602081267224ab869762b4dc7ac271e
Ryan Rackemann <ryanrackemann@gmail.com>
2018-08-29 23:22:26 -0700

    Email Success message
    Added js success message on email submission

---
+++ js/email-success.js
@@ -0,0 +1,6 @@
+function checkUrl(){
+	var url = window.location.href;
+	if(window.location.href.includes("success")){
+		document.getElementById("emailSuccess").innerHTML = "Thank you for your email. I will respond to you as soon as I can.";
+	}
+}
--- js/validation.js
+++
@@ -1,5 +0,0 @@
-function validate(){
-	document.getElementById("response").innerHTML = "<div class=\"w3-panel w3-leftbar w3-border-red w3-pale-red\">" +
-		"<p>I have a thick red top and bottom border and a pale-red background color.</p>" +
-	"</div>";
-}
