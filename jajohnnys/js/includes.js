08b86ed06d21d8731f1fd01cbaa8271fda353ad9
Ryan <ryanrackemann@gmail.com>
2019-01-25 15:16:58 -0800

    css and angular js update
    updated css to justify content properly and condensed css. using angular js for simpler include file system.

--- jajohnnys/js/includes.js
+++ jajohnnys/js/includes.js
@@ -1,6 +1,6 @@
 $(function(){
-	$("#header").load("includes/header.html");
-	$("#nav").load("includes/nav.html");
+	$("header").load("includes/header.html");
+	$("nav").load("includes/nav.html");
 	$("#hours-fb").load("includes/hours-fb.html");
-	$("#footer").load("includes/footer.html");
+	$("footer").load("includes/footer.html");
 });
